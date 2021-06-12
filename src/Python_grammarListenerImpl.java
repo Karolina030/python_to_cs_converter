f77cf8660d9ccc1d3e0ae1185e3aac2ebbf92022
Cornel <31167551+akusmierek@users.noreply.github.com>
2021-06-12 20:14:09 +0200

    Added tabs for out code; fixed if statement brackets

--- src/Python_grammarListenerImpl.java
+++ src/Python_grammarListenerImpl.java
@@ -6,6 +6,7 @@ import static java.util.stream.Collectors.toList;
 public class Python_grammarListenerImpl extends  Python_grammarBaseListener{
     private final CsCodeAssembler csCodeAssembler = new CsCodeAssembler();
     private final Dictionary<String, String> variables = new Hashtable<>();
+    private int tabs = 0;
 
     public CsCodeAssembler getCode(){
         return csCodeAssembler;
@@ -17,101 +18,85 @@ public class Python_grammarListenerImpl extends  Python_grammarBaseListener{
         String varValue = ctx.expr().getText();
 
         if (variables.get(varName) != null) {
-            csCodeAssembler.addExpression(varName + " = " + varValue + ";");
+            addExpression(varName + " = " + varValue + ";");
         } else {
             variables.put(varName, varValue);
-            csCodeAssembler.addExpression("var " + varName + " = " + varValue + ";");
+            addExpression("var " + varName + " = " + varValue + ";");
         }
+    }
+
+    @Override
+    public void enterBody(Python_grammarParser.BodyContext ctx) {
+        tabs++;
+    }
+
+    @Override
+    public void exitBody(Python_grammarParser.BodyContext ctx) {
+        tabs--;
     }
 
     @Override
     public void enterList_assignment_stmt(Python_grammarParser.List_assignment_stmtContext ctx) {
         String varName = ctx.VAR().getText();
-        csCodeAssembler.addExpression("int[] "+varName + " = " + ctx.variable().stream().map(x->x.getText()).collect(toList()).toString().replace("[","{").replace("]", "};"));
+        addExpression("int[] "+varName + " = " + ctx.variable().stream().map(x->x.getText()).collect(toList()).toString().replace("[","{").replace("]", "};"));
     }
 
     @Override
     public void enterIf_stmt(Python_grammarParser.If_stmtContext ctx) {
-        csCodeAssembler.addExpression("if (" + ctx.expr_bool().getText().replace("or", " || ").replace("and", " && ").replace("not ", "!") + ") {");
+        addExpression("if (" + ctx.expr_bool().getText().replace("or", " || ").replace("and", " && ").replace("not ", "!") + ") {");
     }
 
     @Override
     public void exitIf_stmt(Python_grammarParser.If_stmtContext ctx) {
-        csCodeAssembler.addExpression("}");
+        if (ctx.elif_stmt() == null && ctx.else_stmt() == null)
+            addExpression("}");
     }
 
     @Override
     public void enterElif_stmt(Python_grammarParser.Elif_stmtContext ctx) {
-        csCodeAssembler.addExpression("}\nelse if (" + ctx.expr_bool().getText() + ") {");
+        addExpression("}\nelse if (" + ctx.expr_bool().getText() + ") {");
     }
 
     @Override
     public void exitElif_stmt(Python_grammarParser.Elif_stmtContext ctx) {
-        csCodeAssembler.addExpression("}");
+        addExpression("}");
     }
 
     @Override
     public void enterElse_stmt(Python_grammarParser.Else_stmtContext ctx) {
-        csCodeAssembler.addExpression("else {");
+        addExpression("else {");
     }
 
     @Override
     public void exitElse_stmt(Python_grammarParser.Else_stmtContext ctx) {
-        csCodeAssembler.addExpression("}");
+        addExpression("}");
     }
 
     @Override
-    public void enterWhile_stmt(Python_grammarParser.While_stmtContext ctx) { csCodeAssembler.addExpression("while (" + ctx.expr_bool().getText() + ") {"); }
+    public void enterWhile_stmt(Python_grammarParser.While_stmtContext ctx) { addExpression("while (" + ctx.expr_bool().getText() + ") {"); }
 
     @Override
-    public void exitWhile_stmt(Python_grammarParser.While_stmtContext ctx) { csCodeAssembler.addExpression("}"); }
+    public void exitWhile_stmt(Python_grammarParser.While_stmtContext ctx) { addExpression("}"); }
 
 
     @Override
     public void enterFor_stmt(Python_grammarParser.For_stmtContext ctx) {
-        csCodeAssembler.addExpression("foreach (int " + ctx.VAR().get(0) + " in "+ ctx.VAR().get(1) +") {");}
+        addExpression("foreach (int " + ctx.VAR().get(0) + " in "+ ctx.VAR().get(1) +") {");}
 
     @Override
-    public void exitFor_stmt(Python_grammarParser.For_stmtContext ctx) { csCodeAssembler.addExpression("}"); }
+    public void exitFor_stmt(Python_grammarParser.For_stmtContext ctx) { addExpression("}"); }
 
 
     @Override
     public void enterPrint_stmt(Python_grammarParser.Print_stmtContext ctx) {
-        csCodeAssembler.addExpression("Console.WriteLine("+ ctx.expr().getText()+");");
+        addExpression("Console.WriteLine("+ ctx.expr().getText()+");");
     }
 
+    private void addExpression(String expression){
+        csCodeAssembler.addExpression(getTabs() + expression);
+    }
 
-//    private int testDepth = 0;
-//    private StringBuilder test = new StringBuilder();
-//
-//    @Override
-//    public void enterNot_test(Python_grammarParser.Not_testContext ctx) {
-//        testDepth++;
-//
-//        String var = "";
-//        if (ctx.not_test() != null)
-//            var += ctx.not_test();
-//        else
-//            var += ctx.comparison();
-//
-//        test.append("!").append(var);
-//    }
-//
-//    @Override
-//    public void exitNot_test(Python_grammarParser.Not_testContext ctx) {
-//        testDepth--;
-//
-//        if (testDepth-- == 0)
-//
-//    }
-//
-//    @Override
-//    public void enterAnd_test(Python_grammarParser.And_testContext ctx) {
-//        csCodeAssembler.addExpression(ctx.not_test());
-//    }
-//
-//    @Override
-//    public void exitAnd_test(Python_grammarParser.And_testContext ctx) {
-//        csCodeAssembler.addExpression();
-//    }
+    private String getTabs(){
+        return "\t".repeat(tabs);
+    }
 }
